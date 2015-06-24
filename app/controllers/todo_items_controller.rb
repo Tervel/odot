678b42afd1cb17a104a41b85723247a93f650593
Jordan Anslow <jordananslow@gmail.com>
2015-06-25 00:28:27 +1000

    Write tests for todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,5 @@
 class TodoItemsController < ApplicationController
   def index
+    @todo_list = TodoList.find(params[:todo_list_id]) # Grabs from routes, @todo_list is an instance variable (access in view)
   end
 end
