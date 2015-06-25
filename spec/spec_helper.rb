bcd80bbb83141ff5fc5b13cff978d82e956cb8f9
Jordan Anslow <jordananslow@gmail.com>
2015-06-26 03:33:43 +1000

    Add rails DOM ID helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include TodoListHelpers, type: :feature # Include visit_todo_list method
+  config.include RailsDomIdHelper, type: :feature
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
